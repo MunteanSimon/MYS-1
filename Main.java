039b323d20d2da8baf2c5b4194bdd6d21c6325ad
SiMuntea <simon.muntean2@frequentis.com>
2024-08-06 16:17:11 +0300

    total from one number

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        printeven();
+    }
+    public static void printeven(){
+        Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
+        System.out.print("Enter first number- ");
+        int b= sc.nextInt();
+        System.out.print("Enter first number- ");
+        int a= sc.nextInt();
+        int d=a+b;
+        System.out.println("Total= " +d);
+        System.out.println("Thanks");
     }
 }
